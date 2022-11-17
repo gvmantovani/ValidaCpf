adeb51d60cbbec5b365eb05665d75ac88e42dd56
gvman <gvmantovani@hotmail.com>
2022-11-17 19:45:42 -0300

    continuação

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Formula:
 5 * 3  = 15           #    5 *  4 = 20
 0 * 2  = 0            #    0 *  3 = 0
                       # -> 0 *  2 = 0
-         297          #             343
+    297          #             343
 11 - (297 % 11) = 11  #     11 - (343 % 11) = 9
 11 > 9 = 0            #
 Digito 1 = 0          #   Digito 2 = 9
@@ -24,4 +24,34 @@ import re
 print('\n Digite um CPF \n')
 ValorCPF = input('Digite o CPF: ')
 entrada = re.findall("\d", ValorCPF)
+if len(ValorCPF) > 14 or len(entrada) < 11 or len(entrada) > 11:
+    print('CPF INVÁLIDO')
+    if len(ValorCPF) > 14 or len(entrada) < 11 or len(entrada) > 11:
+        print('CPF INVÁLIDO')
+    else:
+        valid = 0
+        for dig in range(0, 11):
+            valid += int(entrada[dig])
+            dig += 1
+        if int(entrada[0]) == valid / 11:
+            print("CPF INVÁLIDO")
+        else:
+            soma = 0
+            count = 10
+            for i in range(0, len(entrada) - 2):
+                soma = soma + (int(entrada[i]) * count)
+                i += 1
+                count -= 1
+            dg1 = 11 - (soma % 11)
+            if dg1 >= 10:
+                dg1 = 0
+            soma = 0
+            count = 10
+            for j in range(1, len(entrada) - 1):
+                soma = soma + (int(entrada[j]) * count)
+                j += 1
+                count -= 1
+            dg2 = 11 - (soma % 11)
+            if dg2 >= 10:
+                dg2 = 0
 # projeto em andamento
